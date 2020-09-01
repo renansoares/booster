259a7835fed82ff53ab4a65d6454f4309f2a6202
juanjoman <j2rgez@gmail.com>
2020-09-01 16:08:52 +0200

    issue-196 WIP

--- packages/cli/test/commands/new.test.ts
+++ packages/cli/test/commands/new.test.ts
@@ -1,5 +1,22 @@
-// import {expect, test} from '@oclif/test'
+import { restore, stub } from 'sinon'
+import * as ProjectChecker from '../../src/services/project-checker'
+import { test, expect } from '@oclif/test'
 
 describe('new', (): void => {
-  // TODO: Write proper tests
+  describe('command', (): void => {
+    stub(ProjectChecker, 'checkItIsABoosterProject').returnsThis()
+    afterEach(() => {
+      restore()
+    })
+    describe('run', () => {
+      context('using --help or -h flags', async () => {
+        test
+          .command(['new:command', '--help'])
+          .stdout()
+          .it('runs without errors', (ctx) => {
+            expect(ctx.stdout).to.not.include('Error: EEXIT: 0')
+          })
+      })
+    })
+  })
 })
